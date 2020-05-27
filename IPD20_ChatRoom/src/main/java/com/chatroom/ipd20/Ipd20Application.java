6fa11fba81957eb2e54d49f2d85eca3e6f97f6b8
W <futurename@gmail.com>
2020-05-27 10:31:46 -0400

    init Spring project

---
+++ IPD20_ChatRoom/src/main/java/com/chatroom/ipd20/Ipd20Application.java
@@ -0,0 +1,15 @@
+package com.chatroom.ipd20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipd20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipd20Application.class, args);
+
+		System.out.println("Running project!!!!!!!!!!!!!!");
+	}
+
+}
