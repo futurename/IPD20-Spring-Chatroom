178fe3cd8b332f3a443f7a0899987fd60a7ca555
heok <heok.ca@gmail.com>
2020-06-01 17:08:33 -0400

    bugfix: favoriteChannels(joint) infinite loop(@Data -> @Getter @Setter) No hashCode()

--- IPD20_ChatRoom/src/main/java/com/chatroom/ipd20/models/UserForm.java
+++ IPD20_ChatRoom/src/main/java/com/chatroom/ipd20/models/UserForm.java
@@ -3,6 +3,7 @@ package com.chatroom.ipd20.models;
 import com.chatroom.ipd20.errors.validator.Email;
 import com.chatroom.ipd20.errors.validator.PasswordConfirm;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -23,4 +24,6 @@ public class UserForm {
     @NotNull
     @Size(min = 5, max = 50)
     private String pass2;
+
+    private MultipartFile file;
 }
